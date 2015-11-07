c78f84f7c3ad693c61d8d9047ef044e724355024
Emin Sh <sharifovemin@outlook.com>
2015-11-07 00:56:40 -0800

    SureFire Report

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -1,13 +1,12 @@
 package core;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static String a = "Testing Junit 4";
+	public static Integer b = 100;
+	public static boolean c = true;
+	public static boolean d = false;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Wetravel Home Page");
+	}
 }
--- src/test/java/core/AppTest.java
+++ src/test/java/core/AppTest.java
@@ -6,68 +6,77 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AppTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		System.out.println("BeforeClass method will be executed before first test method starts");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		System.out.println("AfterClass method will be executed after last test method starts ");
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		System.out.println("Before method will be executed before every test method starts ");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		System.out.println("After method will be executed before every test method starts ");
 	}
-// START TESTING LANDING PAGE LINKS FOR ALL THREE CONDITIONS
+
+	// START TESTING LANDING PAGE LINKS FOR ALL THREE CONDITIONS
 	@Test
-	public void WT_LP_01_01() {
-		System.out.println("Test_01_assertEquals_Positive");
+	public void WT_LP_01_01_Positive() {
+		assertEquals("NOT_SAME_STRING", "Testing Junit 4", core.App.a);
 	}
+
 	@Test
-	public void WT_LP_01_02() {
-		System.out.println("Test_02_assertEquals_Negative");
+	public void WT_LP_01_02_Negative() {
+		assertEquals("NOT_SAME_STRING", "TESTING TestNG 6", core.App.a);
 	}
+
+	@Ignore
 	@Test
-	public void WT_LP_01_03() {
-		System.out.println("Test_03_assertEquals_Ignored");
+	public void WT_LP_01_03_Ignored() {
+		assertEquals("NOT_SAME_STRING", "TESTING JUnit 3", core.App.a);
 	}
+
 	@Test
-	public void WT_LP_02_01() {
-		System.out.println("Test_04_assertSame_Positive");
+	public void WT_LP_02_01_Positive() {
+		assertSame("NOT_SAME_INTEGER", 100, core.App.b);
 	}
+
 	@Test
-	public void WT_LP_02_02() {
-		System.out.println("Test_05_assertSame_Negative");
+	public void WT_LP_02_02_Negative() {
+		assertSame("NOT_SAME_INTEGER", 10, core.App.b);
 	}
+
+	@Ignore
 	@Test
-	public void WT_LP_02_03() {
-		System.out.println("Test_06_assertSame_Ignored");
+	public void WT_LP_02_03_Ignored() {
+		assertSame("NOT_SAME_INTEGER", 101, core.App.b);
 	}
+
 	@Test
-	public void WT_LP_03_01() {
-		System.out.println("Test_07_assertFalse_Positive");
+	public void WT_LP_03_01_False_Positive() {
+		assertFalse("Boolean is False", core.App.c);
 	}
+
 	@Test
-	public void WT_LP_03_02() {
-		System.out.println("Test_08_assertFalse_Negative");
+	public void WT_LP_03_02_False_Negative() {
+		assertFalse("Boolean is False", core.App.d);
 	}
+
 	@Test
-	public void WT_LP_04_01() {
-		System.out.println("Test_09_assertTrue_Positive");
+	public void WT_LP_04_01_True_Positive() {
+		assertTrue("Boolean is True", core.App.d);
 	}
+
 	@Test
 	public void WT_LP_04_02() {
-		System.out.println("Test_10_assertTrue_Negative");
+		assertTrue("Boolean is True", core.App.c);
 	}
 }
